ec6d1e8ec5efa1bd5d408ffff548f0f11d2c8f7a
Iulian1298 <iulianolaru24@yahoo.com>
2019-12-22 19:04:50 +0200

    Update gitignore

---
+++ Python/API/__init__.py
@@ -0,0 +1,14 @@
+import uuid
+import datetime
+from functools import wraps
+import os
+import jwt
+from flask import Flask, request, make_response, jsonify
+from flask_api import status
+from pip._vendor.appdirs import unicode
+from werkzeug.utils import secure_filename
+import base64
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+app.config['IMAGE_FOLDER'] = "..\\"
